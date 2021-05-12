dd70ee23de7ca786426d2483f632c0a7fa60b2f1
3nt3 <gott@3nt3.de>
2021-05-12 06:50:47 +0200

    release 0.7.5

--- frontend/src/Pages/Top.elm
+++ frontend/src/Pages/Top.elm
@@ -69,7 +69,7 @@ view { params } =
                         , padding 5
                         , Border.rounded 5
                         ]
-                        (text "Beta v0.7")
+                        (text "Beta v0.7.5")
                     ]
                 )
 

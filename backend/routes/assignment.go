df30a5f2134a54353752b14c7cfaceae5e64dcfd
3nt3 <gott@3nt3.de>
2021-06-23 03:05:44 +0200

    fix that one bug where null instead of [] is returned

--- backend/routes/assignment.go
+++ backend/routes/assignment.go
@@ -56,6 +56,7 @@ func CreateAssignment(w http.ResponseWriter, r *http.Request) {
 	}
 
 	assignment.DoneBy = []string{}
+	assignment.DoneByUsers = make([]structs.User, 0)
 
 	_ = returnApiResponse(w, apiResponse{
 		Content: assignment.GetClean(),
